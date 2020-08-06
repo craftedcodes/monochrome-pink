3edb91813dcc69c602895f1750f69213bff6f5d3
miss6muffet <65518042+miss6muffet@users.noreply.github.com>
2020-08-07 01:58:24 +0200

    collapse event privacy

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ $("#myCarousel").on("slide.bs.carousel", function () {
     interval: 2000,
   });
 });
+
+$(document).ready(function () {
+  $(".btn-privacy").on("click", function () {
+    $(this).toggleClass("open");
+    $(".privacy").toggleClass("open");
+  });
+  AudioScheduledSourceNode.init({
+    easing: "ease",
+    duration: 1800,
+  });
+});
